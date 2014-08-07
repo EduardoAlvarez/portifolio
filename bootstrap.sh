8a79fb86d86d03c11fc210463e7d75e576534c04
Eduardo Alvarez <eduardo.alvarez@justdigital.com.br>
2014-08-07 11:52:25 -0300

    boot

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
+#apache
 sudo apt-get install -y apache2
-rm -rf /var/www
+#php5
+sudo apt-get install php5 
+#mysql
+sudo apt-get install mysql-server
+
+rmd: -rf /var/www
 ln -fs /vagrant /var/www
